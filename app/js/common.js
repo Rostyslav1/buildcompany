dc8d60dc653ee39c0c1b6751943454ab2682e656
Rostyslav Hanych <rostyslavweb@gmail.com>
2017-09-10 18:39:49 +0300

    New commit

--- app/js/common.js
+++ app/js/common.js
@@ -3,4 +3,34 @@ $(function() {
 	// Scroll to id
     // $("a[rel='m_PageScroll2id']").mPageScroll2id();
 
+    // Owl Carousel
+
+    $('#bottom_carousel').owlCarousel({
+        center: true,
+        items: 1,
+        loop: true,
+        dots: false,
+        nav: true,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        responsive: {
+            768 : {
+                items: 2
+            }
+        }
+
+    });
+
+    // input time
+
+    var timepicker = new TimePicker('time', {
+        lang: 'en',
+        theme: 'dark'
+    });
+    timepicker.on('change', function(evt) {
+
+        var value = (evt.hour || '00') + ':' + (evt.minute || '00');
+        evt.element.value = value;
+
+    });
+
 });
--- gulpfile.js
+++ gulpfile.js
@@ -28,7 +28,9 @@ gulp.task('common-js', function() {
 gulp.task('js', ['common-js'], function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
-		'app/js/common.min.js', // Всегда в конце
+		'app/libs/owl.carousel/dist/owl.carousel.js',
+		'app/libs/page-scroll-to-id/jquery.malihu.PageScroll2id.js',
+		'app/js/common.min.js'
 		])
 	.pipe(concat('scripts.min.js'))
 	// .pipe(uglify()) // Минимизировать весь js (на выбор)
